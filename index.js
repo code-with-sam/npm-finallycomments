e899e26c6aa51831239a42090821ac75a7011ce9
Sam Billingham <sambillie.sb@gmail.com>
2018-03-27 10:45:56 +0100

    generate from steem post

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-iFrameResize( {}, '.finally-comments iframe' );
+// const frame = require('iframe-resizer')
+
+module.exports.generateFromSteemPost = (steemitUrl, options) => {
+  if (!options) options = {}
+  let settings = {
+    message: 'finally-frame-load',
+    reputation: options.reputation || true,
+    profile: options.profile || true,
+    values: options.values || true,
+    generated: false
+  }
+  let iframe = document.createElement('iframe', { scrolling: 'no' })
+  iframe.src = steemitUrl
+  iframe.width = '100%'
+  iframe.style = 'border: none;'
+  iframe.classList.add('finally-frame')
+  return {iframe, settings}
+}
