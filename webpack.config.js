96792c64629ee16a3d991528969462304cf51c1d
Sam Billingham <sambillie.sb@gmail.com>
2018-06-04 08:49:26 +0100

    move src

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const path = require('path');
 module.exports = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'finally.min.js'
